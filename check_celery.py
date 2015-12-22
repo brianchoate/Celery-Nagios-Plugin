a8c2d953962f4debb3e8fd8a3948d6697071a98d
Brian Choate <brian.choate@gmail.com>
2015-12-22 15:14:46 -0800

    Removed some dependencies. Trimmed back to only check status. Updated for flower instead of celerymon.

--- check_celery.py
+++ check_celery.py
@@ -4,83 +4,69 @@
 check_celery.py
 ~~~~~~~~~
 
-This is a monitoring plugin for Nagios NRPE. If required, ensure a copy of this is 
-placed in the following directory on the host machine: /usr/local/nagios/libexec/
+This is a monitoring plugin for Nagios NRPE. If required,
+ensure a copy of this is placed in the following directory on the host machine:
+    /usr/local/nagios/libexec/
 """
 import sys
 import requests
-import simplejson as json
-from NagAconda import Plugin
+from optparse import OptionParser
 
-check_api = Plugin("Used to determine the status of a Celery worker.", "1.0")
 
-check_api.add_option("p", "port", "Port of the Celery host machine serving the Celerymon API. (default: 8989)", default=8989)
-check_api.add_option("h", "host", "Host of the Celery worker instance. (default: http://localhost)", default="http://localhost")
-check_api.add_option("a", "action", "The status check to perform. (nodeup, health)", default="health")
-check_api.add_option("n", "node", "Check if a specified node is up. Used with `nodeup` action. (default: celery.ubuntu)", default="celery.ubuntu")
-check_api.add_option("l", "limit", "Number of tasks in the past to check. (default: 100)", default=100)
+def getOptions():
+    arguments = OptionParser()
+    arguments.add_option("--host", dest="host",
+                         help="Host rabbitmq is running on",
+                         type="string", default="localhost")
+    arguments.add_option("--port", dest="port",
+                         help="RabbitMQ API port",
+                         type="string", default="55672")
 
-check_api.enable_status("warning")
-check_api.enable_status("critical")
+    return arguments.parse_args()[0]
 
-check_api.start()
 
-if check_api.options.action not in ("nodeup", "health"):
-    check_api.unknown_error("unknown action specified %s." % check_api.options.action)
+if __name__ == '__main__':
 
-response = requests.get("%s:%d/api/worker/" % (check_api.options.host, int(check_api.options.port)))
+    options = getOptions()
+    api_path = "api/workers"
 
-try:
-    response.raise_for_status()
-except Exception as e:
-    print "Status Critical, celerymon API not reachable"
-    sys.exit(2)
-
-try:
-    content = json.loads(response.content)
-except Exception as e:
-    check_api.unknown_error("%s health check response was malformed: %s" % (check_api.options.action, e))
-
-if len(content) == 0:
-    print "Status Ok, nothing in celery queue at the moment"
-    sys.exit(0)
-
-if check_api.options.action == "nodeup":
-    response = requests.get("%s:%d/api/worker/%s" % (check_api.options.host, int(check_api.options.port), check_api.options.node))
+    response = requests.get("%s:%d/%s"
+                            % (options.host,
+                               int(options.port),
+                               api_path))
 
     try:
         response.raise_for_status()
     except Exception as e:
-        print "Status Critical, %s node not found" % check_api.options.node
+        print "Status Critical, celerymon API not reachable"
         sys.exit(2)
 
     try:
-        content = json.loads(response.content)
+        content = response.json()
     except Exception as e:
-        check_api.unknown_error("%s health check response was malformed: %s" % (check_api.options.action, e))
-else:
-    response = requests.get("%s:%d/api/tasks/?limit=%d" % (check_api.options.host, int(check_api.options.port), check_api.options.limit))
+        check_api.unknown_error("%s health check response was malformed: %s"
+                                % (check_api.options.action, e))
 
-    try:
-        response.raise_for_status()
-    except Exception as e:
-        print "Status Critical, task list for node %s cannot be retrieved" % check_api.options.node
-        sys.exit(2)
+    if len(content) == 0:
+        print "Status Ok, nothing in celery queue at the moment"
+        sys.exit(0)
 
-    try:
-        content = json.loads(response.content)
-    except Exception as e:
-        check_api.unknown_error("%s health check response was malformed: %s" % (check_api.options.action, e))
+    errormessage = 'Failed workers: '
+    successmessage = 'Succesful workers: '
 
-    failed = []
-    for task in content:
-        if task[1]["failed"]:
-            failed.append(task[0])
+    for workername in content:
+        worker = content[workername]
+        status = worker["status"]
+
+        if status is not True:
+            failed = True
+            errormessage = '{} {},'.format(errormessage, workername)
+        else:
+            successmessage = '{} {},'.format(successmessage, workername)
 
     if failed:
-        print "Status Warning, the last %d tasks for node %s contain failures: %s" % (check_api.options.limit, check_api.options.node, failed)
-        sys.exit(1)
+        print errormessage
+        sys.exit(2)
 
-check_api.set_status_message("Celery health check successful")
-
-check_api.finish()
+    print "Celery health check successful"
+    sys.exit(0)
